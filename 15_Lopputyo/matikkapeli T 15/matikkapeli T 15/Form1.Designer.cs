7ae0383913d914d8bb278c0a284954fa0f753b25
mikko Reini <AD4016@student.jamk.fi>
2023-10-26 11:35:34 +0300

    tehtävä 15 valmis

--- 15_Lopputyo/matikkapeli T 15/matikkapeli T 15/Form1.Designer.cs
+++ 15_Lopputyo/matikkapeli T 15/matikkapeli T 15/Form1.Designer.cs
@@ -32,6 +32,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -70,11 +72,32 @@
             this.label4.TabIndex = 3;
             this.label4.Text = "0";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(512, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Muistipeli";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(675, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Pelaajan 1 vuoro";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -93,5 +116,7 @@
         private Label label2;
         private Label label3;
         private Label label4;
+        private Label label5;
+        private Label label6;
     }
 }
--- 15_Lopputyo/matikkapeli T 15/matikkapeli T 15/Form1.cs
+++ 15_Lopputyo/matikkapeli T 15/matikkapeli T 15/Form1.cs
@@ -9,12 +9,17 @@ namespace matikkapeli_T_15
         int p1p;
         int p2p;
         List<Button> buttons = new List<Button>();
+        List<Button> showbutton = new List<Button>();
         List<string> match = new List<string>();
-        // Listaan nappejen valuet
         List<string> jee = new List<string>()
         {
-             "a","a","b","b","c","c","d","d",
-             "e","e","v","v","w","w","z","z"
+            "a","b","c","d",
+            "e","f","g","h",
+            "i","j","k","l",
+            "m","n","o","p",
+            "q","r","s","t",
+            "u","v","w","x",
+            "y","z","ö","ä"
         };
         private void lauta(int sizeIndex)
         {
@@ -22,7 +27,6 @@ namespace matikkapeli_T_15
             int aY = 0;
             int nextLineIndex = 0;
 
-            // luodaan lauta
             for (int i = 0; i < sizeIndex * sizeIndex; i++)
             {
                 Button butt = new Button();
@@ -31,7 +35,7 @@ namespace matikkapeli_T_15
                 aX += 100;
                 butt.Size = new Size(100, 100);
                 butt.Click += button_Click;
-                butt.Name = i.ToString();
+                butt.Name = "";
                 buttons.Add(butt);
                 if (i == (sizeIndex - 1) + nextLineIndex)
                 {
@@ -41,67 +45,114 @@ namespace matikkapeli_T_15
                 }
             }
         }
-        private void button_rand() 
+        private void button_rand()
         {
+            List<string> characters = new List<string>(jee);
             List<int> availableSpots = new List<int>();
-            for (int i = 0; i < buttons.Count; i++)
+            Random random = new Random();
+            foreach (string character in jee)
             {
-                if (buttons[i].Text == string.Empty)
+                characters.Add(character);
+            }
+            for (int spot = 0; spot < buttons.Count; spot++)
+            {
+                if (buttons[spot].Name == string.Empty)
                 {
-                    availableSpots.Add(i);
+                    availableSpots.Add(spot);
                 }
             }
-            if (availableSpots.Count > 0)
+            for (int i = 0; i < availableSpots.Count; i++)
             {
-                
-                Random random = new Random();
-                int x = 0;
-                for(int a=0; a <= 8; a++) 
+                int randIndex = random.Next(characters.Count);
+                if (availableSpots.Count > 0)
                 {
-                    for (int i = 0; i < 2; i++)
+                    for (int j = 0; j < 2; j++)
                     {
-                        if (i < 1)
+                        int randSpotIndex = random.Next(availableSpots.Count);
+                        int spotIndex = availableSpots[randSpotIndex];
+                        string character = characters[randIndex];
+                        if (buttons[spotIndex].Name != string.Empty)
                         {
-                            x = random.Next(jee.Count);
+                            j--;
+                            return;
                         }
-                        int randIndex = random.Next(availableSpots.Count);
-                        int spotIndex = availableSpots[randIndex];
-                        buttons[spotIndex].Name = jee[x];
-                        buttons[spotIndex].Text = jee[x];
+                        buttons[spotIndex].Name = character;
+                        if (j == 1)
+                        {
+                            characters.RemoveAt(randIndex);
+                        }
+                        availableSpots.RemoveAt(randSpotIndex);
                     }
+                    i--;
                 }
             }
         }
+        
         private void button_Click(object sender, EventArgs e) 
         {
             Button button = sender as Button;
             match.Add(button.Name);
+            button.Text = button.Name;
+            showbutton.Add(button);
             if (match.Count == 2)
             {
                 if (match[0] == match[1])
                 {
                     MessageBox.Show("match");
-                    match.Clear();
-                    if(player == true) 
+                    if (player == true) 
                     {
                         p1p++;
                         label3.Text = p1p.ToString();
-                        player = false;
                     }
                     else
                     {
                         p2p++;
                         label4.Text = p2p.ToString();
-                        player = true;
                     }
-                    
+                    for (int x = 0; x < showbutton.Count; x++)
+                    {
+                        showbutton[x].Text = button.Name;
+                        showbutton[x].Enabled = false;
+                    }
                 }
                 else
                 {
                     MessageBox.Show("no match");
-                    match.Clear();
+                    for(int x = 0; x < showbutton.Count; x++) 
+                    {
+                        showbutton[x].Enabled= true;
+                        showbutton[x].Text = "";
+                    }
+                    if (player == true) 
+                    {
+                        player= false;
+                        label6.Text = "pelaajan 2 vuoro";
+
+                    }
+                    else 
+                    {
+                        player= true;
+                        label6.Text = "pelaajan 1 vuoro";
+                    }
                 }
-                
+                match.Clear();
+                showbutton.Clear();
+            }
+            if (match.Count == 1) 
+            { 
+                button.Enabled = false; 
+            }
+            if(p2p + p1p == 8) 
+            {
+                if(p1p > p2p) 
+                {
+                    MessageBox.Show("Player 1 won");
+                }
+                else 
+                {
+                    MessageBox.Show("Player 2 Won");
+                }
+                Application.Restart();
             }
         }
         public Form1()
